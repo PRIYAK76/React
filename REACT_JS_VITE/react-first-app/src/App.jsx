05dbf9e2549a2f4e3ce99b643ca78269c3d28f0b
Your Name <you@example.com>
2023-08-22 16:16:48 +0530

    g

---
+++ REACT_JS_VITE/react-first-app/src/App.jsx
@@ -0,0 +1,40 @@
+
+import { useState } from "react";
+import "./styles.css"
+
+function App(){
+  const [newItem,setNewItem]= useState("")
+  
+  return( 
+  <>
+      <form className="new-item-form">
+        <div className="form-row p-3 ">
+        <div className=" d-grid ">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+          </div>
+        </div>
+        <div className=" d-grid p-3 ">
+        <button className="btn btn-outline-primary btn-block">Add</button></div>
+      </form>
+      <h1 className="header">Todo list</h1>
+      <ul className="list">
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-outline-danger">Delete</button>
+        </li>
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-outline-danger">Delete</button>
+        </li>
+      </ul>
+  </>
+  )
+}
+export default App;
---
+++ REACT_VITE/my-react-first/src/Message.tsx
@@ -0,0 +1,10 @@
+
+//PascalCasing
+
+function Message(){
+
+    const name='priya';
+    return <h1>Hello World  {name}</h1>;
+}
+
+export default Message;
