5d4d8803d8dc44f06f78e602b3eec487ab7c13b4
Your Name <you@example.com>
2023-08-22 16:33:24 +0530

    c

--- REACT_JS_VITE/react-first-app/src/App.jsx
+++ REACT_JS_VITE/react-first-app/src/App.jsx
@@ -4,14 +4,13 @@ import "./styles.css"
 
 function App(){
   const [newItem,setNewItem]= useState("")
-  
   return( 
   <>
       <form className="new-item-form">
         <div className="form-row p-3 ">
         <div className=" d-grid ">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
+          <input  value={newItem} onChange={e=> setNewItem(e.target.value)} type="text" id="item" />
           </div>
         </div>
         <div className=" d-grid p-3 ">
