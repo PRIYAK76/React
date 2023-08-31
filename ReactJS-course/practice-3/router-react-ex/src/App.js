09137b8e93cd08c8e0f7e5d770dd6b580a3fb955
PRIYADHARSHINI <139319068+PRIYAK76@users.noreply.github.com>
2023-08-31 12:02:41 +0530

    Update App.js

--- ReactJS-course/practice-3/router-react-ex/src/App.js
+++ ReactJS-course/practice-3/router-react-ex/src/App.js
@@ -3,17 +3,20 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import {Home} from "./pages/Home";
 import {Menu} from "./pages/Menu";
 import {Contact} from "./pages/Contact";
+import { Navbar } from "./Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/menu" element={<Menu/>}/>
           <Route path="/contact" element={<Contact/>}/>
+          <Route path="*" element={<h1>Error:404 Page not Found</h1>}/>
         </Routes>
-      </Router>
+    </Router>
     </div>
   );
 }
