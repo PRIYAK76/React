4722d88f1d56e778ff8ae97a49458e7bc1091278
Your Name <you@example.com>
2023-09-06 16:33:38 +0530

    c

--- ReactJS-course/crud-in-todolist/crud-to-do/src/App.js
+++ ReactJS-course/crud-in-todolist/crud-to-do/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import  {Task} from './Task';
+
 function App() {
   const [todoList,setTodoList] = useState([]);
   const [newTask,setNewTask] = useState();
--- react-website/adventurous-website/src/App.js
+++ react-website/adventurous-website/src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
 
 function App() {
   return (
-    <div className="App">
-     hiii </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' exact component={Home}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 export default App;
