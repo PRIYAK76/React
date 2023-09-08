eeb15e1ef19138f17db0c4f5ad9663c5361cff1c
Your Name <you@example.com>
2023-09-08 17:06:30 +0530

    f

--- redux-react/reduxof/src/components/Profile.js
+++ redux-react/reduxof/src/components/Profile.js
@@ -9,7 +9,7 @@ function Profile() {
       <h1>Profile Page</h1>
       <p>Name:{user.name}</p>
       <p>Age:{user.age}</p>
-      <p>Email:{user.email}</p>
+      <p>Email:{user.email}</p> 
     </div>
   );
 }
