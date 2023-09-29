8cf95333f6740fdcc3a0f09890c70c03792b0dd9
Your Name <you@example.com>
2023-09-29 12:07:21 +0530

    commit

---
+++ advanced-project/src/Navigation/Nav.js
@@ -0,0 +1,11 @@
+import './Nav.css';
+
+function Nav() {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  )
+}
+
+export default Nav
---
+++ advanced-project/src/Sidebar/Category/Category.js
@@ -0,0 +1,11 @@
+import "./Category.css";
+
+function Category() {
+  return (
+    <div>
+      category
+    </div>
+  )
+}
+
+export default Category
