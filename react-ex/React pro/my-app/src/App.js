f8bfd12aa503de3442bd7573d80d68d047665518
Your Name <you@example.com>
2023-10-16 16:54:20 +0530

    commit

---
+++ react-ex/React pro/my-app/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './App.css';
+import ReactImageMagnify from "react-image-magnify"
+import img1 from "../src/imagess/website.jpg"
+
+function App() {
+  // const images = "https://assets.website-files.com/58e46336bbd02c1a2dd27afc/59a0e9b9b4ef790001d62853_tiny-png-tiny-jpg.jpg";
+  
+  return (
+    <div style={{width:"900px", height:"100px"}}>
+      <div style={{width:"800px"}}>
+      <ReactImageMagnify {...{
+      smallImage: {
+          alt: 'logo',
+          isFluidWidth: true,
+          src: img1,
+          width: 640,
+          height: 662
+      },
+      largeImage: {
+          src: img1,
+          width: 836,
+          height: 1100
+      },
+      enlargedImagePosition:'over',
+      lensStyle: {backgroundColor: 'rgba(0,0,0,.6'}
+      }} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
